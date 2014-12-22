20b25183bbdec9284ec8b97cd3e86c8f06209b48
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2014-12-22 13:50:38 +0100

    Update save depth as raw

--- BakeCamera.h
+++ BakeCamera.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "kick/kick.h"
+#include <string>
 
 class BakeCamera : public kick::CameraOrthographic {
+    int screenWidth;
     std::shared_ptr<kick::Texture2D> texture;
 public:
-
+    // filename with no file ending
+    std::string filename = "test";
+    bool writeDebugBMP = true;
 
     BakeCamera(kick::GameObject *gameObject, int screenWidth);
 
