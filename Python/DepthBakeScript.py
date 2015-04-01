fa162daafea1c1784cae6924702d673996916c6a
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2015-04-01 15:09:42 +0200

    Update python scripts

--- Python/DepthBakeScript.py
+++ Python/DepthBakeScript.py
@@ -1,2 +1,45 @@
-__author__ = 'mono'
+import ReadDepthBakeOutput
+import sys
+import os
+import subprocess
+import csv
 
+argc = len(sys.argv)
+
+depthBakePath = '/Users/mono/Library/Caches/appCode31/DerivedData/DepthBake-71380c64/Build/Products/Release/DepthBake'
+depthBakeCWD = '/Users/mono/Programming/cpp/DepthBake/DepthBake/'
+depthBakeOutPath = '/Users/mono/Programming/cpp/DepthBake/DepthBake/baked-data/'
+viewAngleFile = '/Users/mono/Programming/cpp/DepthBake/DepthBake/t62.csv'
+modelFile = '/Users/mono/Programming/cpp/DepthBake/DepthBake/t-62.obj'
+
+targetPath = '/Users/mono/Programming/cpp/DepthBake/DepthBake/baked-data/depthbake_hb14931.0016_plane0.raw'
+planePath = '/Users/mono/Programming/cpp/DepthBake/DepthBake/baked-data/depthbake_hb14931.0016_plane1.raw'
+dataPath = '/Users/mono/Programming/cpp/DepthBake/DepthBake/Python/Data/MSTAR/MSTAR_PUBLIC_MIXED_TARGETS_CD1/15_DEG/COL2/SCENE1/T62/HB14931.016'
+if argc > 1:
+    targetPath = sys.argv[1]
+if argc > 2:
+    planePath = sys.argv[2]
+if argc > 3:
+    planePath = sys.argv[3]
+
+def RunDepthBake():
+    # FNULL = open(os.devnull, 'w')    #use this if you want to suppress output to stdout from the subprocess using stdout=FNULL, stderr=FNULL,
+    args = depthBakePath + " -inputUnit mm -outputPath " + depthBakeOutPath + " -viewAngleFile " + viewAngleFile + ' '+modelFile
+    subprocess.call(args, shell=True, cwd=depthBakeCWD)
+
+def Analyze():
+    with open(viewAngleFile, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        first = True
+        for row in spamreader:
+            if first:
+                first = False
+                continue
+            filename = row[0].strip()
+            targetAz = float(row[1].strip())
+            MeasuredDepression = float(row[2].strip())
+
+RunDepthBake()
+#Analyze()
+
+#ReadDepthBakeOutput.ReadDepthBakeOutput(targetPath, planePath, dataPath)
---
+++ Python/ReadDepthBakeOutput.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 22 14:56:51 2014
+
+@author: dmha
+
+Read DeptBake output
+"""
+
+import numpy as np
+import binreader as bin
+import matplotlib.pyplot as plt
+from numpy import log10
+
+#target = np.genfromtxt('C:\Users\DMHA\Documents\Visual Studio 2013\Projects\DepthBake\\test.raw', delimiter=',')
+def ReadDepthBakeOutput(targetPath, planePath, dataPath):
+    target = np.genfromtxt(targetPath, delimiter=',')
+    plane = np.genfromtxt(planePath, delimiter=',')
+    [im, _, hdr] = bin.readMSTAR(dataPath)
+
+    imh = np.size(im,0)
+    imw = np.size(im,1)
+    ma = np.max(target)
+    mi = np.min(target[target>0.0])
+    img=20*log10(abs(im)+1)
+    plt.figure()
+    plt.imshow(img,'gray')
+    plt.clim(0.0,1)
+
+    ind=hdr.find('RangePixelSpacing')+len('RangePixelSpacing')+2
+    step_range = float(hdr[ind:ind+hdr[ind:].find('\n')])
+    ind=hdr.find('CrossRangePixelSpacing')+len('CrossRangePixelSpacing')+2
+    step_cross = float(hdr[ind:ind+hdr[ind:].find('\n')])
+
+    kfactor = round(1 / (step_cross / (20./512) - np.floor(step_cross / (20./512))))
+    columns=np.sum(target,0).nonzero()[0]
+    shadowlen = np.zeros(len(columns))
+    spos = np.zeros(len(columns))
+    targetlen = np.zeros(len(columns))
+
+    #Find Starting position of object and shadow plus the length of object and shadow
+    for i in range(len(columns)):                                    #columns with object
+        shadowpos = max(target[:,columns[i]].nonzero()[0])
+        shadowlen[i] = round((plane[shadowpos,columns[i]]-target[shadowpos,columns[i]])/step_range)
+        targetlen[i] = round((max(target[:,columns[i]]) - min(target[target[:,columns[i]]>0,columns[i]])) / step_range)
+        spos[i] = round((min(target[target[:,columns[i]]>0,columns[i]]) - mi) / step_range)
+
+    objekt = np.zeros([max(spos+targetlen+shadowlen),len(columns)])
+    for i in range(len(columns)):
+        objekt[spos[i]:spos[i]+targetlen[i],i]=1
+        objekt[spos[i]+targetlen[i]:spos[i]+targetlen[i]+shadowlen[i],i]=2
+
+    #create a new array where an even number (div) of columns corresponds to one column in SAR image regarding metric units.
+    div = kfactor * step_cross/(20./512)
+    kron = np.kron(objekt,np.ones(kfactor))
+
+    #now when div columns in kron correspons to one in our SAR image we can find the outline of the object
+    seg = np.zeros([np.size(kron,0),np.round(np.size(kron,1)/div)])
+    for i in range(int(np.size(kron,1)/div)):
+        seg[:,i] = np.amax(kron[:,i*div:(i+1)*div],1)
+    seg = np.flipud(seg)
+
+    #calculate where to place our segmentated object
+    sc = imw/2 - int(np.where(columns==255)[0]*5/div)
+    [x,y] = np.where(seg==1)
+    shadowheight = np.min(x)
+    x = x-shadowheight
+    k=np.zeros(imh-len(np.unique(x)))
+    for i in range(np.size(im,0)-len(np.unique(x))):
+        k[i]=np.sum(im[x+i,y+sc])
+    sr = np.argmax(k)-shadowheight
+
+    #seg's indicies +sr in rows and +sc in columns is the outline of the tank
+    SARseg = np.zeros([np.size(im,0),np.size(im,1)])
+    SARseg[0+sr:np.size(seg,0)+sr,0+sc:np.size(seg,1)+sc] = seg
+
+    plt.scatter(np.where(SARseg==1)[1],np.where(SARseg==1)[0],c='b',s=20)
+    plt.scatter(np.where(SARseg==2)[1],np.where(SARseg==2)[0],c='r',s=20)
+    plt.axis([ 0, 171, 172, 0])
